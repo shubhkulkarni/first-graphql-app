4295455ecf699a074b246d608cd519ee6cf9d4f6
SHUBHAM <shubhamkulkarni.sh@gmail.com>
2021-02-13 12:25:45 +0530

    setup schema

---
+++ src/index.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const { graphqlHTTP } = require("express-graphql");
+const schema = require("./schema/schema");
+const app = express();
+
+app.use("/graphql", graphqlHTTP({ graphiql: true, schema }));
+app.listen(4000, () => console.log("Server is running on 4000 !"));
